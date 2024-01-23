6971ea676310406c2ecde1158e0ccfe299d53ff2
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2024-01-23 13:45:24 +0100

    test: test pod deployment

--- project/run/utils.py
+++ project/run/utils.py
@@ -19,4 +19,45 @@
     # - job delttion
     # - db update
 
+import json
 
+
+dict = {"apiVersion": "apps/v1",
+        "kind": "Deployment",
+        "metadata": {
+                "name": "helloworld"
+            },
+        "spec": {
+            "selector": {
+                "matchLabels": {
+                    "app": "helloworld"
+                },
+            },
+            "replicas": 1,  # tells deployment to run 1 pods matching the template,
+            "template": {  # create pods using pod definition in this template
+                "metadata": {
+                    "labels": {
+                        "app": "helloworld"
+                    },
+                },
+                "spec": {
+                    "containers": [
+                        {
+                            "name": "helloworld",
+                            "image": "karthequian/helloworld:latest",
+                            "ports": [
+                                {
+                                    "containerPort": 80
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+        }
+
+with open('pod.json', 'w') as f:
+    json.dump(dict, f)
+
+# os.system("cat pod.json | kubectl create -f -")
