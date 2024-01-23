385988350cab1c45d1cc059e1e94686629cd279e
davidhieber <david.hieber@uni-tuebingen.de>
2024-01-23 11:23:03 +0100

    fix: structure text

---
+++ project/run/utils.py
@@ -0,0 +1,22 @@
+# pull images form harbor
+
+# validaition only in running image or if posibel befor
+
+# deploy job
+#  - pod creatoin
+#  - give tokens
+#    - data token
+#    - resutlt token
+#    - PO token
+#  - image execution
+
+# get logs
+
+# hypernate not mvp
+
+# therdown
+    # - store logs
+    # - job delttion
+    # - db update
+
+
