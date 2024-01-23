a315e9858bc07a12f22f1e6fea156c20bd57d0f4
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2024-01-23 16:17:43 +0100

    feat: create_deployment implemented

--- project/run/utils.py
+++ project/run/utils.py
@@ -1,63 +1,24 @@
-# pull images form harbor
-
-# validaition only in running image or if posibel befor
-
-# deploy job
-#  - pod creatoin
-#  - give tokens
-#    - data token
-#    - resutlt token
-#    - PO token
-#  - image execution
-
-# get logs
-
-# hypernate not mvp
-
-# therdown
-    # - store logs
-    # - job delttion
-    # - db update
-
-import json
+from kubernetes import client, config
 
 
-dict = {"apiVersion": "apps/v1",
-        "kind": "Deployment",
-        "metadata": {
-                "name": "helloworld"
-            },
-        "spec": {
-            "selector": {
-                "matchLabels": {
-                    "app": "helloworld"
-                },
-            },
-            "replicas": 1,  # tells deployment to run 1 pods matching the template,
-            "template": {  # create pods using pod definition in this template
-                "metadata": {
-                    "labels": {
-                        "app": "helloworld"
-                    },
-                },
-                "spec": {
-                    "containers": [
-                        {
-                            "name": "helloworld",
-                            "image": "karthequian/helloworld:latest",
-                            "ports": [
-                                {
-                                    "containerPort": 80
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-        }
-        }
+def create_deployment(name: str, image: str, ports: list[int], namespace: str = 'default', kind: str = 'Deployment') -> None:
+    config.load_kube_config()
 
-with open('pod.json', 'w') as f:
-    json.dump(dict, f)
+    v1 = client.AppsV1Api()
 
-# os.system("cat pod.json | kubectl create -f -")
+    containers = []
+    container1 = client.V1Container(name=name, image=image, ports=[client.V1ContainerPort(port) for port in ports])
+    containers.append(container1)
+
+    depl_metadata = client.V1ObjectMeta(name=name, namespace=namespace)
+    depl_pod_metadata = client.V1ObjectMeta(labels={'app': name})
+    depl_selector = client.V1LabelSelector(match_labels={'app': name})
+    depl_pod_spec = client.V1PodSpec(containers=containers)
+    depl_template = client.V1PodTemplateSpec(metadata=depl_pod_metadata, spec=depl_pod_spec)
+
+    depl_spec = client.V1DeploymentSpec(selector=depl_selector, template=depl_template)
+    depl_body = client.V1Deployment(api_version='apps/v1', kind=kind, metadata=depl_metadata, spec=depl_spec)
+
+    v1.create_namespaced_deployment(namespace=namespace, body=depl_body)
+
+    # v1.delete_namespaced_deployment(namespace=namespace, name=name)
