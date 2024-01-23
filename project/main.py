17735203d4d2cef18f9e8c27122f1480d3838d2a
davidhieber <david.hieber@uni-tuebingen.de>
2024-01-23 14:35:06 +0100

    feat: main.py base

--- project/main.py
+++ project/main.py
@@ -1,3 +1,16 @@
-import docker
-import os
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--images', help='name of the image that has to be loaded')
+    parser.add_argument('--name', help='name of the deployment ')
+    args = parser.parse_args()
+
+    print(args.images)
+    print(args.name)
+
+
+if __name__ == '__main__':
+    main()
+
 
