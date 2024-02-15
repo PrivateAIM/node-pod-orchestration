4b434ef6aee4dc775fb07af2451c88a2440e23c0
davidhieber <david.hieber@uni-tuebingen.de>
2024-02-15 16:08:48 +0100

    fix: refactor and fix kubernetes.py functions

--- src/utils/kubernetes.py
+++ src/utils/kubernetes.py
@@ -3,6 +3,15 @@ from typing import Optional
 from kubernetes import client, config
 
 
+app_client = client.AppsV1Api()
+
+core_client = client.CoreV1Api()
+
+
+def load_cluster_config():
+    config.load_incluster_config()
+
+
 def create_deployment(name: str, image: str, ports: list[int], namespace: str = 'default') -> list[str]:
     config.load_kube_config()
     v1 = client.AppsV1Api()
@@ -21,33 +30,28 @@ def create_deployment(name: str, image: str, ports: list[int], namespace: str = 
     depl_spec = client.V1DeploymentSpec(selector=depl_selector, template=depl_template)
     depl_body = client.V1Deployment(api_version='apps/v1', kind='Deployment', metadata=depl_metadata, spec=depl_spec)
 
-    v1.create_namespaced_deployment(namespace=namespace, body=depl_body)
+    app_client.create_namespaced_deployment(async_req=False, namespace=namespace, body=depl_body)
 
     return _get_pods(name)
 
 
 def delete_deployment(name: str, namespace: str = 'default') -> None:
-    config.load_kube_config()
-    api_client = client.AppsV1Api()
-
-    api_client.delete_namespaced_deployment(name=name, namespace=namespace)
+    app_client.delete_namespaced_deployment(async_req=False, name=name, namespace=namespace)
 
 
 def get_logs(name: str, pod_ids: Optional[list[str]], namespace: str = 'default') -> list[str]:
-    config.load_kube_config()
-    api_client = client.CoreV1Api()
     # get pods in deployment
-    pods = api_client.list_namespaced_pod(namespace=namespace, label_selector=f'app={name}')
+    pods = core_client.list_namespaced_pod(namespace=namespace, label_selector=f'app={name}')
     if pod_ids is not None:
-        return [api_client.read_namespaced_pod_log(pod.metadata.name, namespace)
-                for pod in pods.items() if pod.metadata.uid in pod_ids]
+        return [core_client.read_namespaced_pod_log(pod.metadata.name, namespace)
+                for pod in pods.items if pod.metadata.uid in pod_ids]
 
-    return [api_client.read_namespaced_pod_log(pod.metadata.name, namespace)
+    return [core_client.read_namespaced_pod_log(pod.metadata.name, namespace)
             for pod in pods.items]
 
 
 def _get_pods(name: str, namespace: str = 'default') -> list[str]:
-    config.load_kube_config()
     api_client = client.CoreV1Api()
 
-    return api_client.list_namespaced_pod(namespace=namespace, label_selector=f'app={name}')
+    return [pod.metadata.name
+            for pod in api_client.list_namespaced_pod(namespace=namespace, label_selector=f'app={name}').items]
