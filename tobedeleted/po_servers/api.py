24a7c13227746476a44ae24137b59a0e442593ba
davidhieber <david.hieber@uni-tuebingen.de>
2024-02-15 16:25:35 +0100

    fix: remove tobedeleted

--- tobedeleted/po_servers/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from ..po_servers.endpoints import analysis, analysis_info
-
-api_router = APIRouter()
-
-api_router.include_router(analysis.router, prefix="/analysisinfo", tags=["AnalysisInfo"])
-api_router.include_router(analysis_info.router, prefix="/analysis", tags=["Analysis"])
--- tobedeleted/po_servers/endpoints/entity.py
+++
@@ -1,5 +0,0 @@
-import pydantic
-
-
-class AnalyseLogsResponse(pydantic.BaseModel):
-    list: pydantic.Any
