e87c8414b0630fa613f51d1d033556c2345d5592
davidhieber <david.hieber@uni-tuebingen.de>
2024-04-22 15:54:04 +0200

    feat: add create keycloak client

--- src/utils/token.py
+++ src/utils/token.py
@@ -1,5 +1,6 @@
 import os
 import requests
+
 from kong_admin_client import Configuration, ApiClient, ConsumersApi, ACLsApi, KeyAuthsApi
 from kong_admin_client.rest import ApiException
 from kong_admin_client.models.create_consumer_request import CreateConsumerRequest
@@ -13,21 +14,18 @@ from kong_admin_client.models.create_key_auth_for_consumer_request import (
 
 def create_tokens(analysis_id: str, project_id: str) -> dict[str, str]:
     tokens = {'DATA_SOURCE_TOKEN': _get_kong_token(analysis_id, project_id),
-            'KEYCLOAK_TOKEN': _get_keycloak_token(analysis_id, project_id)}
+              'KEYCLOAK_TOKEN': _get_keycloak_token(analysis_id)}
 
     return tokens
 
 
-
-def _get_keycloak_token(analysis_id: str, project_id: str) -> str:
-    #TODO create client in keycloak
+def _get_keycloak_token(analysis_id: str) -> str:
     # curl -q -X POST -d "grant_type=client_credentials&client_id=service1&client_secret=9dd01665c2f3f02f93c32d03bd854569f03cd62f439ccf9f0861c141b9d6330e" http://flame-node-keycloak-service:8080/realms/flame/protocol/openid-connect/token
-    client = os.getenv('RESULT_CLIENT_ID')
-    client_secret = os.getenv('RESULT_CLIENT_SECRET')
+
+    client_secret = _create_keycloak_client(analysis_id)
 
     keycloak_url = os.getenv('KEYCLOAK_URL') + "/realms/flame/protocol/openid-connect/token"
-
-    data = {"grant_type": "client_credentials", "client_id": client, "client_secret": client_secret}
+    data = {"grant_type": "client_credentials", "client_id": analysis_id, "client_secret": client_secret}
 
     # get token from keycloak like in the above curl command
     try:
@@ -37,6 +35,39 @@ def _get_keycloak_token(analysis_id: str, project_id: str) -> str:
     except requests.exceptions.RequestException as e:
         print(e)
         return None
+
+
+def _create_keycloak_client(analysis_id: str) -> str:
+    """
+    Create a client in keycloak
+    :return:
+    """
+    keycloak_admin_user = os.getenv('KEYCLOAK_ADMIN_USER')
+    keycloak_admin_password = os.getenv('KEYCLOAK_ADMIN_PASSWORD')
+    keycloak_realm = os.getenv('KEYCLOAK_REALM')
+    keycloak_url = os.getenv('KEYCLOAK_URL')
+
+    # get admin token
+    url_admin_access_token = f"{keycloak_url}/auth/realms/{keycloak_realm}/protocol/openid-connect/token"
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    data = {"grant_type": "password", "client_id": keycloak_admin_user, "client_secret": keycloak_admin_password}
+    response = requests.post(url_admin_access_token, headers=headers, data=data, verify=False)
+    response.raise_for_status()
+    admin_token = response.json()['access_token']
+
+    # create client
+    url_create_client = f"{keycloak_url}/auth/admin/realms/{keycloak_realm}/clients"
+    headers = {"Authorization": f"Bearer {admin_token}"}
+
+    client_data = {
+        "clientId": analysis_id,
+        "publicClient": True,  # Adjust based on your client type
+    }
+
+    response = requests.post(url_create_client, headers=headers, data=client_data , verify=False)
+    response.raise_for_status()
+    print(response.json())
+    return response.json()['client_secret']
 
 
 def _get_kong_token(analysis_id: str, project_id: str) -> str:
