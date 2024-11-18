e8b75454d9b73621b32c0caec0f4e2265b52df68
davidhieber <david.hieber@uni-tuebingen.de>
2024-11-18 14:22:33 +0100

    fix: add connecterror for hub status
    Co-authored-by: Nightknight3000 <alexander.roehl@uni-tuebingen.de>

--- src/status/status.py
+++ src/status/status.py
@@ -161,17 +161,17 @@ def _submit_analysis_status_update(node_analysis_id: str, status: AnalysisHubSta
     :return:
     """
     if status is not None:
-
-        response = asyncio.run(AsyncClient(base_url=os.getenv('HUB_URL_CORE'),
-                                          headers={"accept": "application/json",
-                                                   "Authorization":f"Bearer {get_hub_token()['hub_token']}"})
-                               .post(f'/analysis-nodes/{node_analysis_id}',
-                                     json={"run_status": status},
-                                     headers=[('Connection', 'close')]))
-        #print(f"response status update: {response.json()}")
         try:
+            response = asyncio.run(AsyncClient(base_url=os.getenv('HUB_URL_CORE'),
+                                              headers={"accept": "application/json",
+                                                       "Authorization":f"Bearer {get_hub_token()['hub_token']}"})
+                                   .post(f'/analysis-nodes/{node_analysis_id}',
+                                         json={"run_status": status},
+                                         headers=[('Connection', 'close')]))
+            #print(f"response status update: {response.json()}")
+
             response.raise_for_status()
-        except httpx.HTTPStatusError as e:
+        except (httpx.HTTPStatusError, httpx.ConnectError) as e:
             print(f"Error updating analysis status: {e}")
 
 
