afe26128bc264e91b81441b1da997cd128310f6f
davidhieber <david.hieber@uni-tuebingen.de>
2025-03-19 13:49:26 +0100

    refactor: add print for debuging
    Co-authored-by: Nightknight3000 <alexander.roehl@uni-tuebingen.de>

--- src/resources/database/entity.py
+++ src/resources/database/entity.py
@@ -61,6 +61,7 @@ class Database:
 
     def update_deployment(self, deployment_name: str, **kwargs) -> AnalysisDB:
         deployment = self.get_deployment(deployment_name)
+        print(f"{deployment_name}: {deployment} (isNone: {deployment is None})")
         print(kwargs.items())
         for key, value in kwargs.items():
             print(f"Setting {key} to {value}")
