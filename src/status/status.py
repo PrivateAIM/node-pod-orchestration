4cff4474b9b89156a2d411398580304bcf23f198
davidhieber <david.hieber@uni-tuebingen.de>
2025-04-03 10:52:37 +0200

    refactor: add print
    Co-authored-by: Nightknight3000 <alexander.roehl@uni-tuebingen.de>

--- src/status/status.py
+++ src/status/status.py
@@ -239,6 +239,7 @@ async def refresh_keycloak_token(deployment_name: str, analysis_id: str, token_r
     if token_remaining_time < 1700:
         keycloak_token = get_keycloak_token(analysis_id)
         try:
+            print(f"Refreshing keycloak token for deployment {deployment_name} : {keycloak_token}")
             response = await (AsyncClient(base_url=f'http://nginx-{deployment_name}:80')
                               .post('/analysis/token_refresh',
                                     data={"token": keycloak_token},
