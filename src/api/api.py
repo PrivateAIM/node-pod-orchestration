5656a850170751483ca09abc0adeda9b3108c723
davidhieber <david.hieber@uni-tuebingen.de>
2025-05-07 10:48:40 +0200

    fix: Name Error namespace is part of body

--- src/api/api.py
+++ src/api/api.py
@@ -60,7 +60,7 @@ class PodOrchestrationAPI:
         uvicorn.run(app, host="0.0.0.0", port=8000)
 
     def create_analysis_call(self, body: CreateAnalysis):
-        return create_analysis(body, self.database, namespace)
+        return create_analysis(body, self.database)
 
     def retrieve_history_call(self, analysis_id: str):
         return retrieve_history(analysis_id, self.database)
