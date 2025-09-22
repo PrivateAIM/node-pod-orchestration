9700e61fd728e704af167bb0266373e1b05eed13
davidhieber <david.hieber@uni-tuebingen.de>
2025-09-22 13:33:15 +0200

    fix: handle None log cases in analysis log retrieval and update
    Co-authored-by: Nightknight3000 <alexander.roehl@uni-tuebingen.de>

--- src/resources/database/entity.py
+++ src/resources/database/entity.py
@@ -134,11 +134,15 @@ class Database:
         deployment = self.get_deployments(analysis_id)[0]
 
         if deployment is not None:
-            return deployment.log
+            log = deployment.log
+            if log is not None:
+                return log
         return ""
 
     def update_analysis_log(self, analysis_id: str, log: str) -> None:
-        log = str(self.get_analysis_log(analysis_id)) + "\n" + log
+        latest = self.get_analysis_log(analysis_id)
+        if latest:
+            log = latest + "\n" + log
         self.update_analysis(analysis_id, log=log)
 
     def update_analysis_status(self, analysis_id: str, status: str) -> None:
